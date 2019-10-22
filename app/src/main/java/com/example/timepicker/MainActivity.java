855aa83b3de025cdebe0d04fd9e3f9f74b5f0b1f
utsavshah <utsavshah191@gmail.com>
2019-10-22 23:44:58 +0530

    decimal value format

--- app/src/main/java/com/example/timepicker/MainActivity.java
+++ app/src/main/java/com/example/timepicker/MainActivity.java
@@ -1,41 +1,73 @@
 package com.example.timepicker;
 
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-
+    LinearLayout llView;
     RangeTimePickerDialog garoodaTimePickerDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=findViewById(R.id.btnTimer);
+        llView = findViewById(R.id.llView);
+        button = findViewById(R.id.btnTimer);
+        final EditText editText = (EditText) findViewById(R.id.editText);
 
+        editText.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+                String text = arg0.toString();
 
+                if (text.contains(".") && text.substring(text.indexOf(".") + 1).length() > 2) {
+                    editText.setText(text.substring(0, text.length() - 1));
+                    editText.setSelection(editText.getText().length());
+                }
+            }
 
-
-
-        garoodaTimePickerDialog =new RangeTimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
 
             }
-        },15,22,false);
-        garoodaTimePickerDialog.setMin(10,30);
-        garoodaTimePickerDialog.setMax(13,30);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                garoodaTimePickerDialog.show();
+            public void afterTextChanged(Editable arg0) {
             }
         });
-    }
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                  //  Toast.makeText(getApplicationContext(), "Got the focus", Toast.LENGTH_LONG).show();
+                } else {
+                  String data=  editText.getText().toString();
+                  if (!data.isEmpty()){
+                      double values= Double.parseDouble(data);
+                      editText.setText(""+String.format("%.2f", values));
+                      Log.d("length",""+editText.getText().length());
+                  }
+
+
+
+                   // Toast.makeText(getApplicationContext(), "Lost the focus", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+}
+
 }
